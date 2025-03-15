b20cee65a1ffe41d423124d1cf8e8ca4da7065b8
norstrat <norstrat88@gmail.com>
2025-03-15 15:48:33 +0000

    pushing the source code for the nalinia provacy policy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,90 +5,75 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.page_title_h2}>
+            <h2>Privacy Policy for Nalinia Health and Fitness App</h2>
         </div>
+        <div className={styles.logo_container}>
+
+        <Image
+          className={styles.app_logo}
+          src="/nalinia-app-logo.png"
+          alt="Vercel logomark"
+          width={120}
+          height={120}
+        />
+        <h5 >Update date: 15 March 2025</h5>
+        </div>
+        <div className={styles.policy_container_div}>
+
+          <div className={styles.policy_element}>
+            <h4>1-Title</h4>
+            <p>the privacy policy text, lorum isu ibd srtae etc ...the privacy policy text, lorum isu ibd srtae etc ...
+            the privacy policy text, lorum isu ibd srtae etc ...the privacy policy text, lorum isu ibd srtae etc ...
+            the privacy policy text, lorum isu ibd srtae etc ...the privacy policy text, lorum isu ibd srtae etc ...
+            the privacy policy text, lorum isu ibd srtae etc ...the privacy policy text, lorum isu ibd srtae etc ...</p>
+          </div>
+          <div className={styles.policy_element}>
+            <h4>1-Title</h4>
+            <p>the privacy policy text, lorum isu ibd srtae etc ...</p>
+          </div>
+          <div className={styles.policy_element}>
+            <h4>1-Title</h4>
+            <p>the privacy policy text, lorum isu ibd srtae etc ...</p>
+          </div>
+
+          <div className={styles.policy_element}>
+            <h4>1-Title</h4>
+            <p>the privacy policy text, lorum isu ibd srtae etc ...</p>
+          </div>
+
+          <div className={styles.policy_element}>
+            <h4>1-Title</h4>
+            <p>the privacy policy text, lorum isu ibd srtae etc ...</p>
+          </div>
+
+
+        </div>
+
+
+
+
+
       </main>
+
+
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://play.google.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/google-play.png"
+            alt="google play"
+            width={37}
+            height={37}
           />
-          Learn
+          Get this app on Google play
         </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+
       </footer>
     </div>
   );
